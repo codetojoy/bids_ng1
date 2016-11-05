1a40a6413f4ef008ce03fdce7b69c0658f42cd2e
Michael Easter <codetojoy@gmail.com>
2016-11-05 14:21:45 -0300

    incremental change as part of a larger frenzy of activity

--- run_diff.sh
+++ run_diff.sh
@@ -2,46 +2,29 @@
 
 export BIDS_MASTER=/Users/measter/src/github/codetojoy/bids_ng1
 
-function emitLog {
+function doDiff {
+    diff $1 $2
     if [ $? -ne 0 ]; then echo "TRACER check $1" ; fi
 }
 
-diff app.js $BIDS_MASTER
-emitLog "app.js"
+doDiff app.js $BIDS_MASTER
+doDiff index.html $BIDS_MASTER
 
-diff index.html $BIDS_MASTER
-emitLog "index.html"
+doDiff controllers/configController.js $BIDS_MASTER/controllers
+doDiff controllers/gameController.js $BIDS_MASTER/controllers
+doDiff controllers/mainController.js $BIDS_MASTER/controllers
 
-diff controllers/configController.js $BIDS_MASTER/controllers
-emitLog "controllers/configController.js"
-diff controllers/gameController.js $BIDS_MASTER/controllers
-emitLog "controllers/gameController.js"
-diff controllers/mainController.js $BIDS_MASTER/controllers
-emitLog "controllers/mainController.js"
+doDiff services/DealerService.js $BIDS_MASTER/services
+doDiff services/StrategyService.js $BIDS_MASTER/services
 
-diff services/DealerService.js $BIDS_MASTER/services
-emitLog "services/DealerService.js"
-diff services/StrategyService.js $BIDS_MASTER/services
-emitLog "services/StrategyService.js"
+doDiff views/botHand.html $BIDS_MASTER/views
+doDiff views/buttonBar.html $BIDS_MASTER/views
+doDiff views/gameMain.html $BIDS_MASTER/views
+doDiff views/humanHand.html $BIDS_MASTER/views
+doDiff views/kitty.html $BIDS_MASTER/views
+doDiff views/players.html $BIDS_MASTER/views
+doDiff views/prizeCard.html $BIDS_MASTER/views
+doDiff views/status.html $BIDS_MASTER/views
 
-diff views/botHand.html $BIDS_MASTER/views
-emitLog "views/botHand.html"
-diff views/buttonBar.html $BIDS_MASTER/views
-emitLog "views/buttonBar.html"
-diff views/gameMain.html $BIDS_MASTER/views
-emitLog "views/gameMain.html"
-diff views/humanHand.html $BIDS_MASTER/views
-emitLog "views/humanHand.html"
-diff views/kitty.html $BIDS_MASTER/views
-emitLog "views/kitty.html"
-diff views/players.html $BIDS_MASTER/views
-emitLog "views/players.html"
-diff views/prizeCard.html $BIDS_MASTER/views
-emitLog "views/prizeCard.html"
-diff views/status.html $BIDS_MASTER/views
-emitLog "views/status.html"
-
-diff views/config/buttonBar.html $BIDS_MASTER/views/config
-emitLog "views/config/buttonBar.html"
-diff views/config/configMain.html $BIDS_MASTER/views/config
-emitLog "views/config/configMain.html"
+doDiff views/config/buttonBar.html $BIDS_MASTER/views/config
+doDiff views/config/configMain.html $BIDS_MASTER/views/config
